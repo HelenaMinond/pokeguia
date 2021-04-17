a4b18ffb2cb17f5cd0e8053c2303d17521c62259
Helena Santana <helena.minond@gmail.com>
2021-04-16 23:37:06 -0400

    proyecto 'pokeguia'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <!-- Encabezado -->
+    <div class="encabezadoPokeguia">
+      <img class="encabezadoPokeguia__img" src="./assets/titulo-pokemon.webp" alt="Título Pokemon">
+      <h1>PokeGuía</h1>
+    </div>
+    <!-- Consulta -->
+    <div class="consultaPokeguia">
+      <label>Nombre:</label>
+      <input type="text" v-model="pokemon">
+      <button @click="obtenerPokemon()" >Buscar</button>
+    </div>
+    <!-- Resultado -->
+    <div class="resultadoPokeguia">
+      <img :src="imagenPokemon" alt="Imagen Pokemon">
+      <h2>Movimientos</h2>
+      <ul>
+        <li v-for="(movimiento, indice) in movimientosPokemon" :key="indice">
+          {{movimiento.move.name}}
+        </li>
+      </ul>
+      <h2>Habilidades</h2>
+      <ul>
+        <li v-for="(habilidad, indice) in habilidadesPokemon" :key="indice">
+          {{habilidad.ability.name}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      dataApi: "",
+      pokemon: "pikachu",
+    }
+  },
+  methods: {
+    async obtenerPokemon() {
+      const url = "https://pokeapi.co/api/v2/pokemon/";
+      try {
+        const request = await axios(url + this.pokemon);
+        if(!request) return;
+        this.dataApi = request.data;
+        //console.log(this.dataApi);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created () {
+    this.obtenerPokemon();
+  },
+  computed: {
+    imagenPokemon() {
+      return this.dataApi && this.dataApi.sprites && this.dataApi.sprites.front_default; 
+    },
+    movimientosPokemon(){
+      return this.dataApi.moves;
+    },
+    habilidadesPokemon(){
+      return this.dataApi.abilities;
+    }
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.app{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Arimo', sans-serif;
+  color: rgb(66, 66, 66);
+}
+
+.encabezadoPokeguia {
+  &__img {
+    width: 30%;
+    height: 30%;
+  }
+}
+
+input:focus, input[type]:focus {
+    border: 3px solid lightblue;
+    border-radius: 5px;
+    outline: 0 none;
+}
+
+li {
+  list-style: none;
 }
 </style>
